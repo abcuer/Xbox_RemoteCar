a96548c123d1d247ba91f7e3e95e1c19afe1371e
abcuer <3486243105@qq.com>
2025-11-28 23:46:35 +0800

    手机控制

--- Hardware/hc06.c
+++ Hardware/hc06.c
@@ -13,36 +13,25 @@ void UART_0_INST_IRQHandler(void)
         uint8_t rx_data = DL_UART_Main_receiveData(UART_0_INST);
 		switch(rx_data)
 		{
-			case 0x00:
-				CarStop();
-				break;
-			case 0x01:	//前
-				CarForward();
-				break;	
-			case 0x05:	//后
-				CarBackward();
-				break;
-			case 0x03:	//右
-				CarTurnRight();
-				break;
-			case 0x07:	//左
-				CarTurnLeft();
-				break;
-			case 0x08:	//左前方
-				CarLeftForward();
-				break;
-			case 0x02:	//右前方
-				CarRightForward();
-				break;
-			case 0x06:	//左后方
-				CarLeftBackward();
-				break;
-			case 0x04:	//右后方
-				CarRightBackward();
-				break;
-			default:
-				CarStop();
-				break;
+			case 0x00: 		CarStop(); 			break;
+			/* 前 */
+			case 0x01:		CarForward();		break;
+			/* 后 */
+			case 0x05:		CarBackward();		break;
+			/* 右 */
+			case 0x03:		CarTurnRight();	 	break;
+			/* 左 */	
+			case 0x07:		CarTurnLeft();		break;
+			/* 左前方 */
+			case 0x08:		CarLeftForward();	break;
+			/* 右前方 */
+			case 0x02:		CarRightForward();	break;
+			/* 左后方 */
+			case 0x06:		CarLeftBackward();	break;
+			/* 右后方 */
+			case 0x04:		CarRightBackward();	break;
+			/* 其他 */
+			default:		CarStop();			break;
 		}
     }
 }
